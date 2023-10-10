49e56012a07718c59d883cac0f72a878d732ba72
Lanerdow <lanerdow.anerval@gmail.com>
2023-10-10 20:35:54 +0200

    Affichage du calendrier au mois et à l'année sans saisie utilisateur

--- Calendar_API.py
+++ Calendar_API.py
@@ -1,12 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect
 import calendar
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 @app.route('/calendar/<int:year>/<int:month>', methods=['GET'])
@@ -14,7 +9,7 @@ def get_calendar(year, month):
     if month < 1 or month > 12:
         return jsonify({"error": "Month should be between 1 and 12"}), 400
 
-    return jsonify({"year": year, "month": month, "calendar": calendar.month(year, month)})
+    return jsonify({"calendar": calendar.month(year, month)})
 
 
 if __name__ == '__main__':
--- Calendar_App.py
+++ Calendar_App.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,17 +10,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/calendar/<int:year>/<int:month>', methods=['GET'])
+@app.route('/calendar/<string:year>/<string:month>', methods=['GET'])
 def get_calendar(year, month):
-    api_url = 'http://localhost:5000/calendar/'+year+'/'+month
+    api_url = 'http://localhost:8080/calendar/'+year+'/'+month
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
         data = response.json()
-        print(f"Calendar for {data['year']} and the month {data['month']}:")
         print(data["calendar"])
-        return render_template('calendar.html', year=data['year'], month=data['month'], calendar=data['calendar'])
+        return render_template('calendar.html', year=year, month=month, calendar=data['calendar'])
     else:
         print(f"Error for the request with status code {response.status_code}")
         return response.status_code
